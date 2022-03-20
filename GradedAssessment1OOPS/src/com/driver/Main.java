bcd85f366c5bf7c68e8a46976d62b4f742a6d4c9
Jeyachithra Balaraman <98647655+JeyachithraBalaraman@users.noreply.github.com>
2022-03-20 20:58:25 +0530

    Add files via upload

---
+++ GradedAssessment1OOPS/src/com/driver/Main.java
@@ -0,0 +1,35 @@
+// Author : Jeyachithra Balaraman
+package com.driver;
+
+import com.departments.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// admin department
+		AdminDepartment admin = new AdminDepartment();
+		System.out.println("Welcome to " + admin.departmentName());
+		System.out.println(admin.getTodaysWork());
+		System.out.println(admin.getWorkDeadline());
+		System.out.println(admin.isTodayAHoliday());
+		System.out.println();
+		System.out.println();
+		// HR Department
+		HrDepartment hr = new HrDepartment();
+		System.out.println("Welcome to " + hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday());
+		System.out.println();
+		System.out.println();
+		// Tech Department
+		TechDepartment tech = new TechDepartment();
+		System.out.println("Welcome to " + tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+	}
+
+}
